259937508323b481bfd3a95e0980d80cf47230b5
Art Rask <66017056+artraskMSFT@users.noreply.github.com>
2021-05-25 14:20:58 -0400

    Update FileSelectionModeEnum.cs

--- ThrottledRunner/FileSelectionModeEnum.cs
+++ ThrottledRunner/FileSelectionModeEnum.cs
@@ -1,6 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
 namespace ThrottledRunner
 {
